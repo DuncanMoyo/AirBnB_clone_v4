9429e5c18db264b00a5684a7cf7754fa13ac159b
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-03 11:22:05 +0000

    Task 2 Copied and renamed 0-hbnb.py to 1-hbnb.py Copied and renamed 0-hbnb.html to 1-hbnb.html Updated both renamed files Added 1-hbnb.js

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,23 @@
+// static/scripts/1-hbnb.js
+$(document).ready(function () {
+    let amenitiesDict = {};
+  
+    // Listen for changes on each input checkbox tag
+    $('input[type="checkbox"]').change(function () {
+      const amenityId = $(this).data('id');
+      const amenityName = $(this).data('name');
+  
+      if (this.checked) {
+        // If the checkbox is checked, store the Amenity ID in the dictionary
+        amenitiesDict[amenityId] = amenityName;
+      } else {
+        // If the checkbox is unchecked, remove the Amenity ID from the dictionary
+        delete amenitiesDict[amenityId];
+      }
+  
+      // Update the h4 tag inside the div Amenities with the list of Amenities checked
+      const amenitiesList = Object.values(amenitiesDict);
+      $('.amenities h4').text(amenitiesList.join(', '));
+    });
+  });
+  
