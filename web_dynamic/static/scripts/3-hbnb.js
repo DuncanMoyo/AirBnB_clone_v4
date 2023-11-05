7500c0eddce908f67ec70de5e9c7ec7a647d74fc
Duncan Moyo <duncanfmoyo@gmail.com>
2023-11-05 16:45:41 +0000

    Task 4 - addressed reviews

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,3 +1,5 @@
+/* global $, HOST */
+
 $(document).ready(function () {
   const amenitiesDict = {};
 
@@ -28,26 +30,37 @@ $(document).ready(function () {
     }
   });
 
-  // Request places_search
-  $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    type: 'POST',
-    contentType: 'application/json',
+  $.post({
+	  url: `http://0.0.0.0:5001/api/v1/places_search`,
     data: JSON.stringify({}),
-    success: function (data) {
-      for (const place of data) {
-        $('.places').append('<article><div class="title"><h2>' + place.name +
-          '</h2><div class="price_by_night">' + place.price_by_night +
-          '</div></div><div class="information"><div class="max_guest">' +
-          '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' +
-          place.max_guest + ' Guests</div><div class="number_rooms">' +
-          '<i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' +
-          place.number_rooms + ' Bedrooms</div><div class="number_bathrooms">' +
-          '<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' +
-          place.number_bathrooms + ' Bathroom</div></div>' +
-          '<div class="description">' + place.description + '</div></article>');
-      }
-    }
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    success: (data) => {
+      data.forEach((place) =>
+        $('section.places').append(
+        `<article>
+      <div class="title_box">
+      <h2>${place.name}</h2>
+      <div class="price_by_night">$${place.price_by_night}</div>
+      </div>
+      <div class="information">
+      <div class="max_guest">${place.max_guest} 
+        Guest${place.max_guest !== 1 ? 's' : ''}
+      </div>
+      <div class="number_rooms">${place.number_rooms} Bedroom${
+        place.number_rooms !== 1 ? 's' : ''}</div>
+      <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
+        place.number_bathrooms !== 1 ? 's' : ''}
+      </div>
+      </div> 
+      <div class="description">
+        ${place.description}
+      </div>
+      </article>`
+        )
+      );
+    },
+    dataType: 'json'
   });
 });
-
